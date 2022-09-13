52f8fc9daddc8e19921158c5c3d9dcc59a90906b
ToBinio <tobias.frischmann1@gmail.com>
2022-09-13 14:03:13 +0200

    Initial commit

---
+++ src/main/java/at/tobinio/Main.java
@@ -0,0 +1,10 @@
+package at.tobinio;/**
+ * Created: ${DATE}
+ * @author Tobias Frischmann
+ */
+    
+    public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
